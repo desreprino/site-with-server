8a96cee17b1fcde8a5a1809df4ce0a714dba4d96
agusand <amandreacchi@gmail.com>
2021-11-09 14:09:28 -0300

    Final fixes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,13 @@
-import { useState } from "react";
 import NavBar from "./NavBar";
 import Logo from "./Logo";
-import BurguerMenu from "./BurguerMenu"
+import BurguerMenu from "./BurguerMenu";
 
 const Header = () => {
-
-	const [open, setOpen] = useState(false)
-
 	return (
 		<header className="header">
-			<div className="header__buttons">
-				<Logo />
-				<BurguerMenu open={open} setOpen={setOpen} />
-			</div>
-			<NavBar open={open} />
+			<Logo />
+			<BurguerMenu />
+			<NavBar />
 		</header>
 	);
 };
